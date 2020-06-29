4a2fe3529b8e2b319f6c17833d417d9215cafe18
Matt Nawrocki <mjnrock@umich.edu>
2020-06-29 09:59:07 -0400

    init

--- fka-react/src/lib/hive/MouseNode.js
+++ fka-react/src/lib/hive/MouseNode.js
@@ -5,8 +5,10 @@ export const EnumEventType = {
     MOUSE_MASK: "MouseNode.Mask",
     MOUSE_DOWN: "MouseNode.Down",
     MOUSE_UP: "MouseNode.Up",
+    MOUSE_MOVE: "MouseNode.Move",
+    MOUSE_CONTEXT_MENU: "MouseNode.ContextMenu",
     MOUSE_SELECTION: "MouseNode.Selection",
-    MOUSE_SWIPE: "MouseNode.Selection",
+    MOUSE_SWIPE: "MouseNode.Swipe",
 };
 
 export default class MouseNode extends Hive.Node {
@@ -16,38 +18,53 @@ export default class MouseNode extends Hive.Node {
         this._config = {
             ...this.config,
             allowComplexActions: false,
+            moveRequiresButton: true,
             swipe: {
                 timeout: 500,
                 threshold: 75,
-            }
+            },
+            selection: {
+                timeout: 5000,
+                threshold: 20
+            },
         };
 
-        this.state = {
+        this._state = {
             map: [
                 {
-                    button: 0,
+                    button: 1,
                     flag: 2 << 0,
                     name: "LEFT",
                 },
                 {
-                    button: 1,
+                    button: 2,
                     flag: 2 << 1,
                     name: "MIDDLE",
                 },
                 {
-                    button: 2,
+                    button: 3,
                     flag: 2 << 2,
                     name: "RIGHT",
                 },
             ],
             mask: 0,
-            selection: [],
-            swipe: [],
+            selection: {
+                left: [],
+                middle: [],
+                right: [],
+            },
+            swipe: {
+                left: [],
+                middle: [],
+                right: [],
+            },
         };
 
         if(element) {
             element.onmousedown = e => this.onMouseDown.call(this, e);
             element.onmouseup = e => this.onMouseUp.call(this, e);
+            element.onmousemove = e => this.onMouseMove.call(this, e);
+            element.oncontextmenu = e => this.onContextMenu.call(this, e);
         }
     }
 
@@ -55,7 +72,7 @@ export default class MouseNode extends Hive.Node {
         let mask = this.state.mask;
 
         for(let entry of this.state.map) {
-            if(entry.button === e.button) {
+            if(entry.button === e.which) {
                 if(Bitwise.has(mask, entry.flag)) {
                     mask = Bitwise.remove(mask, entry.flag);
                 } else {
@@ -76,79 +93,109 @@ export default class MouseNode extends Hive.Node {
     get _selection() {
         return {
             begin: (e) => {
-                this.state.selection = [];
-                this.state.selection.push([ e.x, e.y ]);
+                const btn = e.which === 1 ? "left" : (e.which === 2 ? "middle" : (e.which === 3 ? "right" : null));
+
+                if(btn) {
+                    this.state.selection[ btn ] = [];
+                    this.state.selection[ btn ].push([ e.x, e.y, Date.now() ]);
+
+                    setTimeout(() => {
+                        this.state.selection[ btn ] = [];
+                    }, this.config.selection.timeout);
+                }
             },
             end: (e) => {
-                this.state.selection.push([ e.x, e.y ]);
+                const btn = e.which === 1 ? "left" : (e.which === 2 ? "middle" : (e.which === 3 ? "right" : null));
+                
+                if(btn) {
+                    this.state.selection[ btn ].push([ e.x, e.y, Date.now() ]);
 
-                const [ [ x0, y0 ], [ x1, y1 ] ] = this.state.selection;
-                this.dispatch(EnumEventType.MOUSE_SELECTION, {
-                    start: {
-                        x: x0,
-                        y: y0,
-                    },
-                    end: {
-                        x: x1,
-                        y: y1,
-                    },
-                    width: x1 - x0,
-                    height: y1 - y0,
-                });
-                this.state.selection = [];
+                    const [ [ x0, y0, t0 ], [ x1, y1, t1 ] ] = this.state.selection[ btn ];
+                    const dx = x1 - x0;
+                    const dy = y1 - y0;
+                    const dt = t1 - t0;
+
+                    if(dt <= this.config.selection.timeout && (Math.abs(dx) >= this.config.selection.threshold && Math.abs(dy) >= this.config.selection.threshold)) {
+                        this.dispatch(EnumEventType.MOUSE_SELECTION, {
+                            button: btn,
+                            start: {
+                                x: x0,
+                                y: y0,
+                            },
+                            end: {
+                                x: x1,
+                                y: y1,
+                            },
+                            width: x1 - x0,
+                            height: y1 - y0,
+                        });
+                    }
+                    this.state.selection[ btn ] = [];
+                }
             },
         }
     }
     get _swipe() {
         return {
             begin: (e) => {
-                this.state.swipe = [];
-                this.state.swipe.push([ e.x, e.y, Date.now() ]);
+                const btn = e.which === 1 ? "left" : (e.which === 2 ? "middle" : (e.which === 3 ? "right" : null));
+
+                if(btn) {
+                    this.state.swipe[ btn ] = [];
+                    this.state.swipe[ btn ].push([ e.x, e.y, Date.now() ]);
+
+                    setTimeout(() => {
+                        this.state.swipe[ btn ] = [];
+                    }, this.config.swipe.timeout);
+                }
             },
             end: (e) => {
-                this.state.swipe.push([ e.x, e.y, Date.now() ]);
+                const btn = e.which === 1 ? "left" : (e.which === 2 ? "middle" : (e.which === 3 ? "right" : null));
+                
+                if(btn) {
+                    this.state.swipe[ btn ].push([ e.x, e.y, Date.now() ]);
 
-                const [ [ x0, y0, t0 ], [ x1, y1, t1 ] ] = this.state.swipe;
-                const dx = x1 - x0;
-                const dy = y1 - y0;
-                const dt = t1 - t0;
+                    const [ [ x0, y0, t0 ], [ x1, y1, t1 ] ] = this.state.swipe[ btn ];
+                    const dx = x1 - x0;
+                    const dy = y1 - y0;
+                    const dt = t1 - t0;
 
-                if(dt <= this.config.swipe.timeout && (Math.abs(dx) >= this.config.swipe.threshold || Math.abs(dy) >= this.config.swipe.threshold)) {
-                    let dir;
+                    if(dt <= this.config.swipe.timeout && (Math.abs(dx) >= this.config.swipe.threshold || Math.abs(dy) >= this.config.swipe.threshold)) {
+                        let dir;
 
-                    if(Math.abs(dx) >= Math.abs(dy)) {
-                        // X Dominant
-                        if(dx > 0) {
-                            dir = "right";
-                        } else {                            
-                            dir = "left";
+                        if(Math.abs(dx) >= Math.abs(dy)) {
+                            if(dx > 0) {
+                                dir = "right";
+                            } else {                            
+                                dir = "left";
+                            }
+                        } else {
+                            if(dy > 0) {
+                                dir = "down";
+                            } else {                            
+                                dir = "up";
+                            }
                         }
-                    } else {
-                        // Y Dominant
-                        if(dy > 0) {
-                            dir = "down";
-                        } else {                            
-                            dir = "up";
-                        }
+
+                        this.dispatch(EnumEventType.MOUSE_SWIPE, {
+                            button: btn,
+                            start: {
+                                x: x0,
+                                y: y0,
+                            },
+                            end: {
+                                x: x1,
+                                y: y1,
+                            },
+                            magnitude: {
+                                x: dx,
+                                y: dy
+                            },
+                            direction: dir,
+                        });
                     }
-
-                    this.dispatch(EnumEventType.MOUSE_SWIPE, {
-                        start: {
-                            x: x0,
-                            y: y0,
-                        },
-                        end: {
-                            x: x1,
-                            y: y1,
-                        },
-                        magnitude: {
-                            x: dx,
-                            y: dy
-                        },
-                        direction: dir,
-                    });
+                    this.state.swipe[ btn ] = [];
                 }
-                this.state.swipe = [];
             },
         }
     }
@@ -170,5 +217,24 @@ export default class MouseNode extends Hive.Node {
 
         this._selection.end(e);
         this._swipe.end(e);
+    }
+    onMouseMove(e) {
+        e.preventDefault();
+
+        this.updateMask(e);
+
+        if(this.config.moveRequiresButton === true) {
+            if(e.buttons > 0) {
+                this.dispatch(EnumEventType.MOUSE_MOVE, e);
+            }
+        } else {
+            this.dispatch(EnumEventType.MOUSE_MOVE, e);
+        }
+    }
+    onContextMenu(e) {
+        e.preventDefault();
+
+        this.updateMask(e);
+        this.dispatch(EnumEventType.MOUSE_CONTEXT_MENU, e);
     }
 }
