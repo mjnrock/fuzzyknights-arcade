96317f5163e3ca38584c28b9fbe5adf9e3e5049c
Matt Nawrocki <mjnrock@umich.edu>
2020-07-08 13:28:51 -0400

    init

--- index.js
+++
@@ -1,8 +0,0 @@
-import GraphFactory from "./lib/GraphFactory";
-
-const graph = GraphFactory.Generate(1, 1, 4, 4);
-console.log(graph.nodes[ "0.0" ].getTiles());
-// console.log(JSON.stringify(graph, null, 2));
-
-// const node = graph.getNode(0, 0);
-// console.log(node.neighbors(0, 0));
