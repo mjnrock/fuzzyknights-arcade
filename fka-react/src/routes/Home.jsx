7b90c622b6c6a6726e1540fa1e465fe7bb8b5cbe
Matt Nawrocki <mjnrock@umich.edu>
2020-08-13 08:36:46 -0400

    init

--- fka-react/src/routes/Home.jsx
+++ fka-react/src/routes/Home.jsx
@@ -1,22 +1,44 @@
 import React from "react";
 import { useNodeContext } from "@lespantsfancy/hive/lib/react";
-import { Segment, Message } from "semantic-ui-react";
+import { Segment, List, Button, Modal, Icon } from "semantic-ui-react";
 
 import { Context } from "../App";
 import Canvas from "../Canvas";
+
+function ColoredConfig({ setting } = {}) {
+    const { state } = useNodeContext(Context);
+    const value = state.game.setting(setting);
+
+    return (
+        <span
+            style={{
+                fontWeight: "bold",
+                color: value ? "#5c9e6a" : "#993333",
+            }}
+        >{ String(value) }</span>
+    )
+}
 
 export default function Home(props) {
     const { node } = useNodeContext(Context);
 
     return (
-        <Segment>
-            <Message>
-                <Message.Header>Key Bindings</Message.Header>
-                <Message.List>
-                    <Message.Item><kbd>F3</kbd> Toggle Debug Mode</Message.Item>
-                    <Message.Item><kbd>V</kbd> Toggle Life Bars</Message.Item>
-                </Message.List>
-            </Message>
+        <Segment>            
+            <Modal
+                trigger={
+                    <Button icon>
+                        <Icon name="keyboard outline" />
+                    </Button>
+                }
+                header="Key Bindings"
+                content={
+                    <List style={{ padding: 16 }}>
+                        <List.Item><kbd>F3</kbd> Toggle Debug Mode [ <ColoredConfig setting={ "isDebugMode" } /> ]</List.Item>
+                        <List.Item><kbd>V</kbd> Toggle Life Bars [ <ColoredConfig setting={ "showNameplates" } /> ]</List.Item>
+                    </List>
+                }
+            />
+
             <Canvas src={ node.canvas } />
         </Segment>
     )
