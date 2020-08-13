8ec5a3b38a6ed9e35f39068eee9b0c7001a7bffd
Matt Nawrocki <mjnrock@umich.edu>
2020-08-13 08:25:46 -0400

    init

--- fka-react/src/routes/Home.jsx
+++ fka-react/src/routes/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNodeContext } from "@lespantsfancy/hive/lib/react";
-import { Segment } from "semantic-ui-react";
+import { Segment, Message } from "semantic-ui-react";
 
 import { Context } from "../App";
 import Canvas from "../Canvas";
@@ -10,7 +10,13 @@ export default function Home(props) {
 
     return (
         <Segment>
-            <p>{ node.fps }</p>
+            <Message>
+                <Message.Header>Key Bindings</Message.Header>
+                <Message.List>
+                    <Message.Item><kbd>F3</kbd> Toggle Debug Mode</Message.Item>
+                    <Message.Item><kbd>V</kbd> Toggle Life Bars</Message.Item>
+                </Message.List>
+            </Message>
             <Canvas src={ node.canvas } />
         </Segment>
     )
